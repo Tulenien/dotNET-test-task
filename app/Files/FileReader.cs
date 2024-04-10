42164a776bbc9a7b19622a8f6aceda93755609cf
Tulenien <timofey.evsigneev@gmail.com>
2024-04-10 19:37:25 +0200

    feat: implement reading files

---
+++ app/Files/FileReader.cs
@@ -0,0 +1,24 @@
+﻿namespace app.Files;
+
+public static class FileReader
+{
+    public static List<string> ReadFileContent(string path)
+    {
+        List<string> result = [];
+        try
+        {
+            using StreamReader sr = new(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                result.Add(line);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        return result;
+    }
+}
---
+++ app/Program.cs
@@ -0,0 +1,23 @@
+﻿using app.Files;
+using app.Filters;
+
+namespace app;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string pattern = @"^(?:[0-9]{1,3}\.){3}[0-9]{1,3} \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$";
+        try
+        {
+            List<string> result = FileReader.ReadFileContent("./TestFiles/CorrectLineFile.txt");
+            IFilter filter = new LineFormatFilter(pattern);
+            Console.WriteLine(filter.Check(result[0]));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+    }
+}
